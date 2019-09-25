9e146d57bf88cabdf2893efa74c4d8494bbdd80e
ipcjs <gipcjs@gmail.com>
2019-09-25 15:46:45 +0800

    By default, using commonjs, it is more friendly to IDE

--- rollup.config.js
+++ rollup.config.js
@@ -5,27 +5,33 @@ import replace from 'rollup-plugin-replace'
 import path from 'path'
 
 export default {
-    input: path.resolve(__dirname, './src/index.js'),
-    output: {
-        file: path.resolve(__dirname, './index.js'),
-        format: 'umd'
+  input: path.resolve(__dirname, './src/index.js'),
+  output: [
+    {
+      file: path.resolve(__dirname, './index.umd.js'),
+      format: 'umd'
     },
-    name: 'BMapLib.MarkerClusterer',
-    plugins: [
-        babel({
-          exclude: '**',
-          presets: ['es2015-rollup']
-        }),
-        nodeResolve({
-            jsnext: true,
-            main: true,
-            extensions: [ '.js', '.json' ]
-        }),
-        commonjs({
-            include: 'node_modules/**'
-        }),
-        replace({
-            'process.env.NODE_ENV': "'production'"
-        })
-    ]
+    {
+      file: path.resolve(__dirname, './index.common.js'),
+      format: 'cjs'
+    }
+  ],
+  name: 'BMapLib.MarkerClusterer',
+  plugins: [
+    babel({
+      exclude: '**',
+      presets: ['es2015-rollup']
+    }),
+    nodeResolve({
+      jsnext: true,
+      main: true,
+      extensions: ['.js', '.json']
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': "'production'"
+    })
+  ]
 }
